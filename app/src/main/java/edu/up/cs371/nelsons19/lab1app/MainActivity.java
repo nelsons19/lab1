63b9c1b630313834d8bcb36e8f08a1a8255a36a0
nelsons19 <nelsons19@up.edu>
2019-01-14 20:04:17 -0800

    initial commit

---
+++ app/src/main/java/edu/up/cs371/nelsons19/lab1app/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.up.cs371.nelsons19.lab1app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
